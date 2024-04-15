88f8e0901600c23492f0ae297e114ff197560517
Ricwxiao <cxiao4@uw.edu>
2024-04-14 21:57:41 -0700

    finished basic functions

--- Store/Store/main.swift
+++ Store/Store/main.swift
@@ -7,13 +7,68 @@
 
 import Foundation
 
-protocol SKU {}
+protocol SKU {
+    var name : String {get}
+    func price() -> Int
+    
+}
 
-class Item {}
+class Item: SKU {
+    var _name: String
+    var name: String {
+        get {
+            return self.name
+        }
+        set {
+            self.name = _name
+        }
+    }
+    var priceTag: Int
+    init(_ name: String, _ price: Int) {
+        _name = name
+        self.priceTag = price
+    }
+    func price() -> Int {
+        return self.priceTag
+    }
+}
 
-class Receipt {}
+class Receipt {
+    var itemList: [Item]
+    
+    init(_ skuList: [Item]) {
+        self.itemList = skuList
+    }
+    init() {
+        self.itemList = []
+    }
+    
+    func items() -> [SKU] {
+        return self.itemList
+    }
+    func output() {
+        for item in self.itemList {
+            print(item.name)
+        }
+    }
+    func add(_ item: Item) {
+        self.itemList.append(item)
+    }
+}
 
-class Register {}
+class Register {
+    var receipt: Receipt
+    init() {
+        self.receipt = Receipt()
+    }
+    
+    func scan(_ item: Item) {
+        receipt.add(item)
+    }
+    func subtotal() {
+        return
+    }
+}
 
 class Store {
     let version = "0.1"
